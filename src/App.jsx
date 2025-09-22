c394739c903982608e6b656e8a680c7cbe2c97c9
miyakeippei <bcf0420@gmail.com>
2025-09-23 00:47:34 +0900

    認証実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import {Amplify} from "aws-amplify";
+import {withAuthenticator} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
-function App() {
-  const [count, setCount] = useState(0)
+function App({signOut,user}) {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {user ?(
+        <>
+        <h3>{user.username}</h3>
+        <button onClick={signOut}>サインアウト</button>
+        </>
+      ):(
+        <h3>権限なし</h3>
+      )}
       <div className="test">towa</div>
       <div className="test">ippei</div>
       <div className="test">とわとわ</div>
@@ -35,4 +29,4 @@ function App() {
   )
 }
 
-export default App
+export default withAuthenticator(App);
