befdd6bb8e02743d76dbfddb912a938885add4bd
miyakeippei <bcf0420@gmail.com>
2025-09-22 22:45:11 +0900

    branch_testより追加

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="test">towa</div>
       <div className="test">ippei</div>
     </>
   )
