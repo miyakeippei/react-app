1f3d3ef3a06fdb24e15c4a1e4bf779cfca0401a9
miyakeippei <bcf0420@gmail.com>
2025-09-23 12:58:31 +0900

    元に戻す

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Amplify, Auth, API, graphqlOperation } from "aws-amplify";
+import { Amplify } from "aws-amplify";
 import {withAuthenticator} from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import awsExports from "./aws-exports";
-import { listContracts } from "./graphql/queries";
+
+// import { API, graphqlOperation } from "aws-amplify";
+// import { listContracts } from "./graphql/queries";
+
 Amplify.configure(awsExports);
 
 function App({signOut,user}) {
-  const [contracts, setContracts] = useState([]);
-
-  useEffect(() => {
-    async function fetchContracts() {
-      try {
-        //  ログインユーザーの契約番号を取得
-        const currentUser = await Auth.currentAuthenticatedUser();
-        const contractNumber = currentUser.attributes["custom:contractNumber"];
-
-        //  契約番号で契約データを検索
-        const result = await API.graphql(
-          graphqlOperation(listContracts, {
-            filter: { contractNumber: { eq: contractNumber } }
-          })
-        );
-
-        setContracts(result.data.listContracts.items);
-      } catch (error) {
-        console.error("契約データ取得エラー:", error);
-      }
-    }
-
-    fetchContracts();
-  }, []);
 
 
   return (
